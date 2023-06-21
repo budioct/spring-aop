10fe9f2cc481fb3d5e624489009e44637af6f2db
Budhi Octaviansyah <budioct@gmail.com>
2023-06-21 20:56:22 +0700

    pengenalan AOP

---
+++ src/test/java/com/tutorial/aop/pengenalan/PengenalanAOP.java
@@ -0,0 +1,28 @@
+package com.tutorial.aop.pengenalan;
+
+public class PengenalanAOP {
+
+    /**
+     * Pengenalan AOP (intinya membantu penulisan log satu kali saja. yang bisa melintasi antar method dan object)
+     * ● AOP atau singkatan dari Aspect Oriented Programming
+     * ● AOP melengkapi OOP (Object Oriented Programming) dalam membuat kode program
+     * ● Saat kita menggunakan OOP,
+     *   - inti dari modularity kode adalah class,
+     *   sedangkan di AOP, inti dari
+     *   - modularity kode adalah aspect
+     * ● Aspect memungkinkan modularity dari concerns (perhatian), yang bisa melintasi berbagai jenis tipe data dan object
+     *
+     * Spring AOP
+     * ● Spring memiliki dua fitur AOP, yang pertama implementasi sendiri menggunaka Spring AOP, yang
+     *   kedua menggunakan library AOP yang bernama AspectJ
+     * ● Pada kelas ini, kita akan membahas yang menggunakan library AspectJ, karena banyak digunakan
+     *   di Spring, salah satunya untuk fitur Declarative Transaction Management
+     *
+     *   Membingungkan
+     * ● AOP biasanya akan sangat membingungkan ketika pertama kali mengenal istilahnya
+     * ● Tidak perlu khawatir, karena itu memang sudah biasa untuk programmer yang biasa menggunakan OOP
+     * ● Namun nanti ketika kita sudah lihat contoh-contoh penggunaan AOP, maka kita bisa dengan
+     *   mudah mengerti tentang AOP
+     */
+
+}
---
+++ src/test/java/com/tutorial/aop/pengenalan/PengenalanAspectJ.java
@@ -0,0 +1,20 @@
+package com.tutorial.aop.pengenalan;
+
+public class PengenalanAspectJ {
+
+    /**
+     * AspectJ
+     * ● AspectJ adalah salah satu library yang banyak digunakan untuk implementasi Aspect Oriented Programming
+     * ● Untungnya, Spring sudah terintegrasi dengan baik dengan AspectJ, sehingga kita tidak perlu
+     *   banyak melakukan pengaturan AspectJ secara manual
+     * ● https://www.eclipse.org/aspectj/
+     *
+     * Apakah Harus Belajar AspectJ Dulu?
+     * ● Sebelum belajar Spring AOP, apakah kita perlu belajar Library AspectJ terlebih dahulu?
+     * ● Tidak, di kelas ini saya akan bahas dari awal sehingga kita tidak perlu belajar Library AspectJ terlebih dahulu
+     * ● Namun jika tertarik untuk mendalami tentang AspectJ, bisa belajar di halaman dokumentasi
+     *   resminya :
+     * ● https://www.eclipse.org/aspectj/doc/released/progguide/index.html
+     */
+
+}
